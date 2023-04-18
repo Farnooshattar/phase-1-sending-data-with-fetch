8c269eac60f6327210852957a04f27223fb4d5a8
Farnoosh Tavakkoli <attarfarnoosh@gmail.com>
2023-04-18 09:54:30 -0400

    complete all

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Add your code here
+// // Add your code here
+function submitData(name,email){
+
+  const formData = {
+    "name": name,
+    "email": email,
+  };
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+   return fetch("http://localhost:3000/users", configurationObject)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    console.log(object);
+    document.body.innerHTML=object.id
+  })
+  .catch(function (error) {
+    alert("Bad things! Ragnarők!");
+    console.log(error.message);
+    document.body.innerHTML=error.message
+  });
+ // return fetch("http://localhost:3000/users")
+  
+}
+
+
+  
